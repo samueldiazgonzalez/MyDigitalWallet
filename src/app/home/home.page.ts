39d25f2bdb8b4c50fc21dacf21de98de5a95c96a
Samuel Diaz Gonzalez <samydiaz006@gmail.com>
2026-04-20 01:51:29 -0500

    modificaciones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../core/services/auth';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: false
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  userName = '';
+  saldo = 0;
+  mostrarSaldo = true;
+  tarjetas: any[] = [];
+  transacciones: any[] = [];
+  uid = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    this.authService.getUser().subscribe(user => {
+      if (!user) {
+        this.router.navigate(['/login']);
+        return;
+      }
+      this.uid = user.uid;
+      this.authService.getUserProfile(user.uid).subscribe((perfil: any) => {
+        if (perfil) {
+          this.userName = perfil.nombre;
+          this.saldo = perfil.saldo || 0;
+        }
+      });
+    });
+  }
+
+  toggleSaldo() {
+    this.mostrarSaldo = !this.mostrarSaldo;
+  }
+
+  goToAddCard() {
+    this.router.navigate(['/add-card']);
+  }
+
+  goToPayment() {
+    this.router.navigate(['/payment']);
+  }
+
+  async logout() {
+    await this.authService.logout();
+  }
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  userName = '';
+  saldo = 0;
+  mostrarSaldo = true;
+  tarjetas: any[] = [];
+  transacciones: any[] = [];
+  uid = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
+    this.authService.getUser().subscribe(user => {
+      if (!user) {
+        this.router.navigate(['/login']);
+        return;
+      }
+      this.uid = user.uid;
+      this.authService.getUserProfile(user.uid).subscribe((perfil: any) => {
+        if (perfil) {
+          this.userName = perfil.nombre;
+          this.saldo = perfil.saldo || 0;
+        }
+      });
+    });
   }
 
+  toggleSaldo() {
+    this.mostrarSaldo = !this.mostrarSaldo;
+  }
+
+  goToAddCard() {
+    this.router.navigate(['/add-card']);
+  }
+
+  goToPayment() {
+    this.router.navigate(['/payment']);
+  }
+
+  async logout() {
+    await this.authService.logout();
+  }
 }
